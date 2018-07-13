aab4040901701437e8fe2fc986e4b1c00f52686c
Calm <1calm2@gmail.com>
2018-07-13 20:50:03 +0300

    ~

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -6,11 +6,14 @@ import com.badlogic.gdx.physics.box2d.Body;
 
 class Player {
     int x,y;
+    float factor = 1;
     Sprite sprite;
     Body body;
 
     Player(int x, int y, String sprite){
         this.sprite = new Sprite(new Texture(sprite));
+//        factor = 64/((this.sprite.getWidth() + this.sprite.getHeight())/2);
+        this.sprite.setScale(factor);
         this.x = x;
         this.y = y;
     }
