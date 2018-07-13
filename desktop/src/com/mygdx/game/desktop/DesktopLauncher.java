726fe5e922ae1b952cd29faa196c0cdceb01a52c
Calm <1calm2@gmail.com>
2018-07-13 19:11:32 +0300

    test commit

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
+	//хуй
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Hello blyad";
